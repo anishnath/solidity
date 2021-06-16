8f98ca15142701a61e00f3b7cb563dcf23016669
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:17:43 +0530

    Create arrayAddExample.sol

---
+++ arrayAddExample.sol
@@ -0,0 +1,24 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract arrayAddExample{
+    uint[] myArray;
+    
+    function addToArray() external {
+        myArray.push(1);     // add an element to the array
+        myArray.push(3);     // add another element to the array
+        
+        myArray[0];          // get the element at key 0 or first element in the array  
+        myArray[0] = 20;     // update the first element in the array
+    }
+    
+    
+    function getIndex(uint index) public view returns (uint)
+    {
+        return myArray[index];
+    }
+    
+    function deleteIndex(uint index) public
+    {
+        delete myArray[index];
+    }
+}
