4d0fec57b07f2a2664dafd0da0bb6cfc78cefbe8
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:16:34 +0530

    Create MemoryAndStorage.sol

---
+++ MemoryAndStorage.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.5.0 <0.7.0;
+contract MemoryAndStorage {
+
+ mapping(uint => Book) books;
+ 
+ struct Book{
+     uint id;
+     uint balance;
+     string author;
+ }
+
+ function addBook(uint id, uint balance,string memory author) public {
+     books[id] = Book(id, balance,author);   
+ }
+
+function updateBalanceinMemory(uint id, uint balance) public {
+     Book memory Book = books[id];
+     Book.balance = balance;
+ }
+ 
+ function updateBalancebyStorage(uint id, uint balance)  public {
+     Book storage Book = books[id];
+     Book.balance = balance;
+ }
+ 
+function getBalance(uint id) public view returns (uint) {
+    return books[id].balance;
+} 
+
+}
