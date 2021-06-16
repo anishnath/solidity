b1460c0bac6bfeb3b6beb458092e91be2107270a
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:13:50 +0530

    Create leftShiftExample.sol

---
+++ leftShiftExample.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract leftShiftExample {
+
+  function leftShift ()  public pure returns ( uint8 ) {
+
+      uint8 a = 8 ; // 0b00001000 
+      uint8 c = a <<  2 ; // 0b00100000 
+      return c; // 32
+  }
+}
