c8b1e063f6b5f317f49f32ea22d9eea4c4593364
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:26:32 +0530

    Create structureGlobalExample.sol

---
+++ structureGlobalExample.sol
@@ -0,0 +1,21 @@
+
+pragma solidity >=0.5.0 <0.9.0;
+   struct Person
+{
+   string name;
+   uint citNo;
+ }
+
+contract structureGlobalExample{
+ 
+    Person person;
+    function addToStruct(string memory name, uint citNo) public
+    {
+        person.name=name;
+        person.citNo=citNo;
+    }
+    function getStruct() public view returns (string memory, uint)
+    {
+        return ( person.name, person.citNo );
+    }
+}
