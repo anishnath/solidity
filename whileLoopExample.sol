e0cb643bb94cd2ea67c57ca741d6666403af35c2
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 15:47:33 +0530

    Create whileLoopExample.sol

---
+++ whileLoopExample.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.5.0 <0.7.0;
+contract whileLoopExample{
+    
+    // Declaring state variable
+    uint8 j = 0;
+    
+    function whileLoop() public  returns(uint8){
+    uint8 k = 0;    
+    while(j < 5) {
+        k = k+5;
+        j++;
+     }
+      return k;
+    }
+}
