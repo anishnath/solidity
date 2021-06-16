70dee2c56288dce884751afa308224aa827af613
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:15:30 +0530

    Create refTypeExample.sol

---
+++ refTypeExample.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract refTypeExample {
+
+    string public  _name;
+    
+    function Person() {
+        _name = "xumo";
+    }
+
+    function f() {
+        
+        modifyName(_name);
+    }
+
+    function modifyName(string storage name)  {
+    
+        var name1 = name;    //等价与string name1 = name;
+        bytes(name1)[0] = 'X';
+    }
+}
+string public  _name;
+
+function Person() {
+    _name = "xumo";
+}
+
+function f() {
+    
+    modifyName(_name);
+}
+
+function modifyName(string storage name)  {
+
+    var name1 = name;    //等价与string name1 = name;
+    bytes(name1)[0] = 'X';
+}
