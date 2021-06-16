14b2fe185ff7c090b82ae9a1807319a70c285299
Anish Nath <anish2good@yahoo.co.in>
2021-06-16 14:14:33 +0530

    Create conditionOpExample.sol

---
+++ conditionOpExample.sol
@@ -0,0 +1,40 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract conditionOpExample {
+    
+  // Supported Operators <=, <, ==, !=, >=And>
+  
+  address address1;
+  address address2;
+  address address3;
+    
+    // <=，<，==，!=，>=和>
+    
+   constructor() public {
+        address1 = 0x8696DeABcdA95734871653C2c3055ae8690f5352;
+        address2 = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+        address3 = 0x8696DeABcdA95734871653C2c3055ae8690f5352;
+    }
+  
+
+  function gtOrEqTo ()  public view returns ( bool ) {
+      return address1 >= address2;
+  }
+  
+  function ltOrEqTo ()  public view returns ( bool ) {
+       return address1 <= address2;
+  }
+  
+  function gt ()  public view returns ( bool ) {
+      return address1 > address2;
+  }
+  
+  function lt ()  public view returns ( bool ) {
+     return address1 > address2;
+  }
+  
+  function eq ()  public view returns ( bool ) {
+     return address1 == address3;
+  }
+  
+}
