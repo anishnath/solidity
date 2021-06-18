478c9b57cd5823ca822139654669783272ba79bf
Anish Nath <anish2good@yahoo.co.in>
2021-06-18 12:50:48 +0530

    Create modifierExample.sol

---
+++ modifierExample.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.25 <0.7.0;
+
+contract modifierExample {
+  address public owner;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    require(msg.sender == owner, "Not owner");
+    _;
+  }
+
+  constructor() public {
+    owner = address(0x123);
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+}
