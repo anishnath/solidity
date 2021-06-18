1662de7c68f93370abd677635a6f12e1b52642f6
Anish Nath <anish2good@yahoo.co.in>
2021-06-18 12:49:39 +0530

    Create inheritanceExample03.sol

---
+++ inheritanceExample03.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.4.25 <0.7.0;
+contract Animal {
+  uint internal a; 
+  uint public b;
+  uint private c;
+  
+    function setA(uint  x) private {
+        a=x;
+    }
+
+    function setB(uint  x) external{
+        b=x;
+    }
+    
+    function setC(uint  x)  public {
+        c=x;
+    }
+    
+    function setD(uint  x)  internal {
+        c=x;
+    }
+}
+// use of is keyword to perform inheritance
+contract Dog is Animal {
+  
+  constructor ()  public{
+      setC(10); // Pass  Derived contract can access parent public functions
+      setD(10);  //Pass  Derived contract can access parent internal functions
+  //  setA(10);  // Error-`private` function:  cannot be called by derived contracts or external accounts
+  //  setB(10);  // Error-`external` function: only by external accounts
+       
+       a=10; // internal variable   
+       b=10; // public variable
+   //  c=10; //Error-private variable 
+    
+    }
+  
+}
