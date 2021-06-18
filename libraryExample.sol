1e388ae1b7d2331fd8f139929fad37154cee915f
Anish Nath <anish2good@yahoo.co.in>
2021-06-18 12:48:01 +0530

    Create libraryExample.sol

---
+++ libraryExample.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+library MyMathLibrary {
+  
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "Adition overflow");
+        return c;
+    }
+  
+}
+
+contract Number {
+  using MyMathLibrary for uint;
+  function testAdd(uint x, uint y) public pure returns (uint) {
+        return x.add(y);
+    }
+}
+
