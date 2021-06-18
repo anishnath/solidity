2b1f66a355d1a0d9e4acc829ef1a56f22b3ac09d
Anish Nath <anish2good@yahoo.co.in>
2021-06-18 12:46:56 +0530

    Create errorHandlingExample.sol

---
+++ errorHandlingExample.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.4.22 <0.8.0;
+contract errorHandlingExample{
+    
+ function div(uint256 a, uint256 b) pure public returns (uint256) {
+    assert(b > 0); // Solidity automatically throws when dividing by 0
+    return a / b;
+  }
+  
+  function div2(uint256 a, uint256 b) pure public returns (uint256) {
+    require(b > 0, "cant divide by zero"); // Solidity automatically throws when dividing by 0
+    return a / b;
+  }
+  
+  function div3(uint256 a, uint256 b) pure public returns (uint256) {
+    if(b==0)
+    {
+        revert();
+    }
+    return a / b;
+  }
+}
+contract HasAnOwner {
+    address owner;
+    
+    function onlyOwnersRevert() public{ 
+        if (msg.sender != owner) { revert("something bad"); }
+        // do something only the owner should be allowed to do
+    }
+    
+    function onlyOwnersAssert() public{ 
+        assert(msg.sender == owner);
+        // do something only the owner should be allowed to do
+    }
+    
+     function onlyOwnersRequire() public{ 
+        require(msg.sender == owner);
+        // do something only the owner should be allowed to do
+    }
+}
